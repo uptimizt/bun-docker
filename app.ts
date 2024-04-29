0d07fc45d3f796429c9b5d684fc520fef67ba247
Antony I <1852897+uptimizt@users.noreply.github.com>
2024-04-29 15:03:38 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ const NODE_ENV = process.env.NODE_ENV ?? "development";
 const server = Bun.serve({
   port: PORT,
   fetch() {
-    return new Response("Welcome to Bun!");
+    return new Response("Welcome to Bun! Hello World!");
   },
 });
 
